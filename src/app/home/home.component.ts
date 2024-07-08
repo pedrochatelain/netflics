39c321b117ec8536fdf9dacc77b80235f87814e8
pedrochatelain <pedrochatelain@gmail.com>
2024-07-08 19:00:21 -0300

    add estrenos

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, Input, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +6,48 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild("container")
+  container!: ElementRef;
+
+  @ViewChild("next")
+  prox!: ElementRef;
+
+  
+  @ViewChild("anterior")
+  anterior!: ElementRef;
+
+  coef = 0;
+  cantEstrenos = 3;
+  numberOfClicksOnProx = 0;
+  numberOfClicksOnAnterior = 0;
+
+
+
+  ngAfterViewInit() {
+    this.anterior.nativeElement.classList.add("display-none")
+    var movies = this.container.nativeElement.querySelectorAll(".movie");
+    this.prox.nativeElement.addEventListener('click', () => {
+      this.anterior.nativeElement.classList.remove("display-none")
+      this.coef += 100;
+      movies.forEach((element:any) => {
+        element.style.transform = `translateX(${-this.coef}%)`
+      });
+      if (this.coef == 200) {
+        this.prox.nativeElement.classList.add("display-none")
+      }
+    });
+
+    this.anterior.nativeElement.addEventListener('click', () => {
+      this.prox.nativeElement.classList.remove("display-none")
+      this.coef -= 100
+      movies.forEach((element:any) => {
+        element.style.transform = `translateX(${- this.coef}%)`
+      });
+      if (this.coef == 0) {
+        this.anterior.nativeElement.classList.add("display-none")
+      } 
+    })
+
+  }
 
 }
